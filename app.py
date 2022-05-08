581400547e8871a909ffaa626f56b19e85ea2f23
XxMemo55xX <s20431@pjwstk.edu.pl>
2022-05-09 01:44:38 +0200

    updates

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
+
 from flask import Flask, session, redirect, url_for
 from home import home
 
 app = Flask(__name__)
 app.register_blueprint(home)
-
+app.config['SECRET_KEY'] = 'diploma_secret_key'
 
 @app.errorhandler(404)
 def page_not_found(e):
--- home.py
+++ home.py
@@ -11,9 +11,9 @@ def index():
     return render_template('home.html')
 
 
-@home.route('/upload_files')
+@home.route('/upload_files', methods=["GET", "POST"])
 def upload():
-    path = '/module/files/upload'
+    path = 'C:/Users/wujec/Desktop/Szymek/CodeProjects/diploma/files/upload'
     if request.method == "POST":
         if request.files:
             if len(os.listdir(path)) != 0:
@@ -31,28 +31,29 @@ def upload():
                 data_filename = upload_filename
                 data_file_path = path + "/" + data_filename
 
-                data_tab = pd.read_excel(data_file_path, 'Data')
+                data_tab_headers = pd.read_excel(data_file_path, 'Data', header=None)
+                data_tab_value = pd.read_excel(data_file_path, 'Data')
                 reader = pd.read_excel(data_file_path, 'Data', header=None, usecols='A:G')
                 data_col_count = len(reader.columns)
 
                 if data_col_count == 7:
-                    name = (data_tab.iat[0, 0])
-                    address = (data_tab.iat[0, 1])
-                    city = (data_tab.iat[0, 2])
-                    zip_code = (data_tab.iat[0, 3])
-                    lat = (data_tab.iat[0, 4])
-                    long_lat = (data_tab.iat[0, 5])
-                    nip = (data_tab.iat[0, 6])
+                    name = (data_tab_headers.iat[0, 0])
+                    address = (data_tab_headers.iat[0, 1])
+                    city = (data_tab_headers.iat[0, 2])
+                    zip_code = (data_tab_headers.iat[0, 3])
+                    lat = (data_tab_headers.iat[0, 4])
+                    long_lat = (data_tab_headers.iat[0, 5])
+                    nip = (data_tab_headers.iat[0, 6])
 
                     if name == "Name" and address == "Address" and city == "City" and zip_code == "Zip-code" and lat == "Lat" and long_lat == "Long Lat" and nip == "NIP":
-                        for record in range(len(data_tab.index)):
-                            name_value = (data_tab.iat[record, 0])
-                            address_value = (data_tab.iat[record, 1])
-                            city_value = (data_tab.iat[record, 2])
-                            zip_code_value = (data_tab.iat[record, 3])
-                            lat_value = (data_tab.iat[record, 4])
-                            long_lat_value = (data_tab.iat[record, 5])
-                            nip_value = (data_tab.iat[record, 6])
+                        for record in range(len(data_tab_value.index)):
+                            name_value = (data_tab_value.iat[record, 0])
+                            address_value = (data_tab_value.iat[record, 1])
+                            city_value = (data_tab_value.iat[record, 2])
+                            zip_code_value = (data_tab_value.iat[record, 3])
+                            lat_value = (data_tab_value.iat[record, 4])
+                            long_lat_value = (data_tab_value.iat[record, 5])
+                            nip_value = (data_tab_value.iat[record, 6])
                     else:
                         message = "Please use the official version of the template file available on main page"
                 else:
